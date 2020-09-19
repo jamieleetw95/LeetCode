339c965fbe5a7556e125199bd7253cbdf2d4eb4c
jamie <jamieleetw95@gmail.com>
2020-09-19 08:38:57 +0800

    add：[15. 3Sum]add file

---
+++ Solution/3Sum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.Solution
+{
+    /// <summary>
+    /// https://leetcode.com/problems/3sum/
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        bool IsZeroByThreeSum(int a, int b, int c)
+        {
+            return (a + b + c) == 0 ? true : false;
+        }
+
+        public void ThreeSumTest()
+        {
+            List<int> input = new List<int>() { -1, 0, 1, 2, -1, -4 };
+            List<List<int>> output = new List<List<int>>() {
+                new List<int>() {-1, -1, 2},
+                new List<int>() {-1, 0, 1 }
+            };
+            var result = ThreeSum(input.ToArray());
+
+
+
+
+            input = new List<int>();
+            output = new List<List<int>>();
+            result = ThreeSum(input.ToArray());
+
+
+            input = new List<int>() { 0 };
+            output = new List<List<int>>() {};
+            result = ThreeSum(input.ToArray());
+        }
+    }
+}
+
