a86f90dc4d0950386e354a7d4863ca11c581164a
jamie <b95170080@gmail.com>
2020-09-20 15:49:02 +0800

    add：add files

---
+++ Solution/ContainerWithMostWater.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode
+{
+    partial class Solution
+    {
+        public int ContainerWithMostWater(int[] inputs)
+        {
+            int result = 0;
+            int max = inputs.Length - 1;
+            int tempLen;
+            var orderArray = OrderArray(inputs);
+            int tempValue;
+            int lastVaule = 0;
+            int lastLen = 0;
+            int lastT = 0;
+
+            for (int i = 0; i <= max-1; i++)
+            {
+                var temp1 = orderArray.ElementAt(i);
+
+                for (int j = i+1; j <= max; j++)
+                {
+                    var temp2 = orderArray.ElementAt(j);
+                    if(temp2.Value== lastVaule)
+                    {
+                        continue;
+                    }
+
+                    tempLen = Math.Abs(temp1.Key - temp2.Key);
+
+                    if (tempLen < lastLen)
+                    {
+                        continue;
+                    }
+                    tempValue = temp2.Value * tempLen;
+                    if(tempValue>= result)
+                    {
+                        result = tempValue;
+                    }
+                    
+                }
+            }            
+
+            return result;
+        }
+
+        private IOrderedEnumerable<KeyValuePair<int,int>> OrderArray(int[] inputs)
+        {
+            int i = 0;
+            return inputs
+                .ToDictionary(x => i++, y => y)
+                .OrderByDescending(x=>x.Value).ThenBy(x=>x.Key);
+        }
+
+        public void ContainerWithMostWaterTest(int[] inputs, int anwser)
+        {
+
+            int result = ContainerWithMostWater(inputs);
+            Console.WriteLine(string.Format("ContainerWithMostWater test {0}，input：{1}，answer：{2}，myoutput：{3}"
+                , result == anwser
+                , string.Join(",", inputs.Select(x => x.ToString()))
+                , anwser
+                , result));
+        }
+    }
+}
---
+++ Solution/MyAtoi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeeCode
+{
+    partial class Solution
+    {
+        public int MyAtoi(string str)
+        {
+            int result = 0;
+
+            var tempStr = str.TrimStart(new char[] { ' ' });
+            if (tempStr.Length==0)
+            {
+                return result;
+            }
+            
+            var match = Regex.Match(tempStr, @"(-|\+)?\d+");
+            
+            if (!match.Success || tempStr.IndexOf(match.Value) > 0)
+            {               
+                return result;
+            }
+
+            double l1; 
+
+            if (double.TryParse(match.Value, out l1) 
+                && l1 != 0)
+            {
+                double temp2 = ((double)l1 / Int32.MinValue);
+
+                result = (temp2 > 1)
+                        ? Int32.MinValue
+                    : (temp2 <= -1) 
+                    ? Int32.MaxValue
+                        : (int)l1;
+            }
+
+            return result;
+        }
+
+        internal void MyAtoiTest(string v1, int v2)
+        {
+            int result = MyAtoi(v1);
+            Console.WriteLine(string.Format("MyAtoi test {0}，input：{1}，answer：{2}，myoutput：{3}"
+                , result == v2
+                , v1
+                , v2
+                , result));
+        }
+
+        public void MyAtoiTest()
+        {
+            MyAtoiTest("   -42", -42);
+            MyAtoiTest("4193 with words", 4193);
+            MyAtoiTest("words and 987", 0);
+            MyAtoiTest("-91283472332", -2147483648);
+            MyAtoiTest("   +0 123", 0);
+            MyAtoiTest("-2147483647", -2147483647);
+            MyAtoiTest("-2147483649", -2147483648);
+            MyAtoiTest("2147483648", 2147483647);
+            MyAtoiTest("20000000000000000000", 2147483647);
+        }
+    }
+}
